ebf1cd3cec288958b6bcb495f58ea741b4ef066e
mabdelaal <Malzoo2@hotmail.com>
2022-09-15 09:33:54 +0300

    add 3 classes

--- src/com/abdelaal/HashSetTest.java
+++ src/com/abdelaal/HashSetTest.java
@@ -22,9 +22,6 @@ public class HashSetTest {
 		}
 		
 		bf.close();
-		
-		
-
 	}
 
 }
---
+++ src/com/abdelaal/InheritanceTest.java
@@ -0,0 +1,29 @@
+package com.abdelaal;
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+	void sing(){
+		System.out.println("I am flying");
+	}
+}
+public class InheritanceTest {
+	
+
+	 public static void main(String args[]){
+
+		  Bird bird = new Bird();
+		  bird.walk();
+		  bird.fly();
+	      bird.sing();
+		
+	   }
+
+}
